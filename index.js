c715405f82db1c4a0e9b02a50059c594cd84c83a
A VENKATA SIVASANKAR <97689510+AVENKATASIVASANKAR@users.noreply.github.com>
2022-05-24 03:05:05 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+
+
+var countDown=setInterval(function(){
+    var RandomNumber=Math.floor(Math.random()*256);
+    document.querySelector('body').style.backgroundColor="rgb("+ Math.floor(Math.random()*256)+" ,"+ Math.floor(Math.random()*256)+" ,"+ 155+")"
+    
+},1000);
+
+// btn.addEventListener('click',function(){
+//     btn.style.background=
+// })
+// btn.addEventListener('load',onclick);
